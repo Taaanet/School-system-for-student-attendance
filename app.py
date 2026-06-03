706d856dd11ca8633dfd5b4b1f11d7f69ba6352d
Taaanet <93224771+Taaanet@users.noreply.github.com>
2026-06-03 09:53:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# الصفحة الرئيسية
 @app.route("/")
 def home():
-    return "نظام حضور الطلاب يعمل بنجاح"
+    return render_template("index.html")
+
+# صفحة مسح QR
+@app.route("/scan")
+def scan():
+    return render_template("scan.html")
 
 if __name__ == "__main__":
     app.run()
