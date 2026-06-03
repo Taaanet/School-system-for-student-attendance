f5f2fe4bc850a480d1e86eaf03e6f86b6a858b1b
Taaanet <93224771+Taaanet@users.noreply.github.com>
2026-06-03 10:32:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template
 
-app = Flask(**name**)
+app = Flask(__name__)
 
 # الصفحة الرئيسية
-
 @app.route("/")
 def home():
-return render_template("index.html")
+    return render_template("index.html")
 
 # صفحة مسح QR
-
 @app.route("/scan")
 def scan():
-return render_template("scan.html")
+    return render_template("scan.html")
 
-if **name** == "**main**":
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
